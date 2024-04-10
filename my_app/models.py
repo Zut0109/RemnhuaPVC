11be9eaaeded4b4e6898a0e27a2b69cfb768e0bd
Zut0109 <hoainguyen.010901@gmail.com>
2024-04-10 09:31:20 +0700

    Add model Item, PictureItem

--- my_app/models.py
+++ my_app/models.py
@@ -14,3 +14,17 @@ class User(models.Model):
     phone = models.CharField(max_length=13)
     role = models.ForeignKey(Role, on_delete=models.CASCADE)
     is_active = models.BooleanField(default=True)
+
+class Item(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=99)
+    size = models.CharField(max_length=12)
+    color = models.CharField(max_length=12)
+    description = models.CharField(max_length=100)
+
+class PictureItem(models.Model):
+    id = models.AutoField(primary_key=True)
+    path = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
