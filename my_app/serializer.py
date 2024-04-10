ea7c16fc76c61de3a80b6a8825e44f2aac12f069
Zut0109 <hoainguyen.010901@gmail.com>
2024-04-10 09:53:19 +0700

    Add serilizer for model Item, PictureItem

--- my_app/serializer.py
+++ my_app/serializer.py
@@ -6,3 +6,13 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'password', 'email', 'password', 'address', 'phone', 'role', 'is_active']
+
+class ItemSerilizer(serializers.ModelSerializer):
+    class Meta:
+        model = Item
+        fields = ['id', 'name', 'price', 'size', 'color', 'description']
+
+class PictureItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PictureItem
+        fields = ['id', 'path', 'description','item']
