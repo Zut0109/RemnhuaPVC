5948e1bf668b42789cde5dd01dfff91893172905
Zut0109 <hoainguyen.010901@gmail.com>
2024-04-12 23:39:10 +0700

    Add admin Items

--- my_app/admin.py
+++ my_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from my_app.models import User,Role
+from my_app.models import User, Role, Item
 
 admin.site.register(User)
 admin.site.register(Role)
+admin.site.register(Item)
