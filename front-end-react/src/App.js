731b953223b54e41fa1e99a74ec09801a0bb6dc1
Zut0109 <hoainguyen.010901@gmail.com>
2024-04-24 04:12:07 +0700

    New Template for User

--- front-end-react/src/App.js
+++ front-end-react/src/App.js
@@ -16,57 +16,38 @@ import Item from "./pages/items";
 import ItemList from "./components/ItemList";
 import Test from "./pages/test";
 import TestUser from "./pages/testUser";
+import Header from "./components/Header";
+import Banner from "./components/Banner";
+import Features from "./components/Features";
+import Categories from "./components/Categories";
+import NewItems from "./components/NewItems";
+import Footer from "./components/Footer";
+import Login from "./pages/login";
+import Account from "./pages/account";
 
 function App() {
 	return (
 		<Router>
+			<Header/>
 			<Navbar/>
-			<section className="container mx-auto mt-10">
-				<div className="relative overflow-hidden">
-					<img src="https://www.canva.com/create/banners/" alt="Banner"
-						 className="w-full h-full object-cover"/>
-					<div
-						className="absolute top-0 left-0 w-full h-full flex items-center justify-center bg-gray-900 opacity-50">
-						<h1 className="text-white font-bold text-3xl">Tiêu đề banner</h1>
-					</div>
-				</div>
-
-				<div className="flex mt-10 ml-10">
-					<main className="w-3/4 mr-10">
-						<Routes>
-							<Route exact path="/" element={<Home/>}/>
-							<Route exact path="/item" element={<Item/>}/>
-							<Route path="/itemlist" element={<ItemList/>}/>
-							<Route path="/test" element={<TestUser/>}/>
-							<Route
-								path="/contact"
-								element={<Contact/>}
-							/>
-							<Route path="/blogs" element={<Blogs/>}/>
-							<Route
-								path="/sign-up"
-								element={<SignUp/>}
-							/>
-						</Routes>
-					</main>
-
-					<aside className="w-1/4">
-						<ul>
-							<li>
-								<a href="#">Tiêu đề 1</a>
-							</li>
-							<li>
-								<a href="#">Tiêu đề 2</a>
-							</li>
-							<li>
-								<a href="#">Tiêu đề 3</a>
-							</li>
-						</ul>
-					</aside>
-				</div>
-			</section>
-
-			<h1>This is footer</h1>
+			<Routes>
+				<Route exact path="/" element={<Home/>}/>
+				<Route exact path="/item" element={<Item/>}/>
+				<Route path="/itemlist" element={<ItemList/>}/>
+				<Route path="/test" element={<TestUser/>}/>
+				<Route path="/account" element={<Account/>}/>
+				<Route path="/login" element={<Login/>}/>
+				<Route
+					path="/contact"
+					element={<Contact/>}
+				/>
+				<Route path="/blogs" element={<Blogs/>}/>
+				<Route
+					path="/sign-up"
+					element={<SignUp/>}
+				/>
+			</Routes>
+			<Footer/>
 		</Router>
 	);
 }
--- front-end-react/src/pages/index.js
+++ front-end-react/src/pages/index.js
@@ -1,11 +1,18 @@
 // Filename - pages/index.js
 
 import React from "react";
+import Banner from "../components/Banner";
+import Categories from "../components/Categories";
+import Features from "../components/Features";
+import NewItems from "../components/NewItems";
 
 const Home = () => {
 	return (
 		<div>
-			<h1>Welcome to GeeksforGeeks</h1>
+			<Banner/>
+			<Categories/>
+			<Features/>
+			<NewItems/>
 		</div>
 	);
 };
